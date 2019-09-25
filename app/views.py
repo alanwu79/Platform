9786184fce38171f7c8e34c3ec46b7ba4f73461c
alanwu79 <chun@chunde-MacBook-Pro.local>
2019-09-26 01:24:01 +0800

    ref twtrubiks

--- app/views.py
+++ app/views.py
@@ -1,25 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
-from django.conf import settings
-from django.shortcuts import render_to_response
-from django.contrib.gis.maps.google.gmap import GoogleMap
-from django.contrib.gis.maps.google.overlays import GMarker, GEvent
 
-def google_map(request):
-    points = [
-        {'lat':'35.42',     'lng': '139.42', 'href':'http://en.wikipedia.org/wiki/Tokyo'},
-        {'lat':'51.30',     'lng':   '0.73', 'href':'http://en.wikipedia.org/wiki/London'},
-        {'lat':'40.43',     'lng': '-74.0',  'href':'http://en.wikipedia.org/wiki/New_York_City'},
-        {'lat':'34.03',     'lng':'-118.15', 'href':'http://en.wikipedia.org/wiki/Los_Angeles'},
-        {'lat':'36.774402', 'lng':'-119.755405', 'href':'http://en.wikipedia.org/wiki/Fresno'},]
-    markers = []
-    for point in points:
-        marker = GMarker('POINT(%s %s)' % (point['lng'], point['lat']))
-        event = GEvent('click', 'function() { location.href = "%s"}' % point['href'])
-        marker.add_event(event)
-        markers.append(marker)
-    google = GoogleMap(center=(0,0), zoom=1, markers=markers,
-                       key=settings.GOOGLE_MAPS_API_PASSWORD)
-    return render_to_response('google_map.html',
-                              {'google': google,})
+def hello_world(request):
+    return HttpResponse("Hello World!")
